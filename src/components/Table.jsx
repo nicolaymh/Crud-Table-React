db3956adcd406f1574d392c123b08efa3a6a655b
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-07-07 17:29:14 -0500

    Finished styles for Table.css

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,26 +4,27 @@ import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
 
 export const Table = () => {
    return (
-      <div>
-         <table>
+      <div className="table-wrapper">
+         <table className="table">
             <thead>
                <tr>
                   <th>Page</th>
-                  <th>Description</th>
+                  <th className="expand">Description</th>
                   <th>Status</th>
                   <th>Actions</th>
                </tr>
             </thead>
+
             <tbody>
                <tr>
                   <td>Home</td>
                   <td>This is the main page</td>
                   <td>
-                     <span>Live</span>
+                     <span className="label label-live">Live</span>
                   </td>
                   <td>
-                     <span>
-                        <BsFillTrashFill />
+                     <span className="actions">
+                        <BsFillTrashFill className="delete-btn" />
                         <BsFillPencilFill />
                      </span>
                   </td>
@@ -33,11 +34,25 @@ export const Table = () => {
                   <td>Page 2</td>
                   <td>This is the second page</td>
                   <td>
-                     <span>Draft</span>
+                     <span className="label label-draft">Draft</span>
                   </td>
                   <td>
-                     <span>
-                        <BsFillTrashFill />
+                     <span className="actions">
+                        <BsFillTrashFill className="delete-btn" />
+                        <BsFillPencilFill />
+                     </span>
+                  </td>
+               </tr>
+
+               <tr>
+                  <td>Page 3</td>
+                  <td>This is the third page</td>
+                  <td>
+                     <span className="label label-error">Draft</span>
+                  </td>
+                  <td>
+                     <span className="actions">
+                        <BsFillTrashFill className="delete-btn" />
                         <BsFillPencilFill />
                      </span>
                   </td>
